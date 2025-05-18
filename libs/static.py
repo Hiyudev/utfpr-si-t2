96ee65cdb28d19dadecbe2ce0b101140df050b42
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2025-05-18 18:57:01 -0300

    docs: add more comments

--- libs/static.py
+++ libs/static.py
@@ -1,13 +1,13 @@
 # Validação cruzada
-# Porcentagem de dados utilizados para validação
+# Porcentagem de exemplos utilizados para compor o conjunto de validação
 ARG_VALIDATION_PERCENTAGE = 0.5
-# Número de repetições para o K-Fold
+# Número de repetições para a validação cruzada por k-repetições
 ARG_KFOLD_REPETITIONS = 5
 
 # Algoritmos
 # O numero de árvores no Random forest 
 ARG_RANDOM_FOREST_TREES = 10
-# Porcentagem de features a serem utilizadas em cada árvore
+# Porcentagem de atributos a serem utilizadas em cada árvore
 ARG_RANDOM_MAX_FEATURES = 0.5
-# Porcentagem de amostras a serem utilizadas em cada árvore
+# Porcentagem de exemplos a serem utilizadas em cada árvore
 ARG_RANDOM_MAX_SAMPLES = 0.2
